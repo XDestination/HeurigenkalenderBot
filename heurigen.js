0eeb8b366ce1ddc5bb84ba3bc1e12f9fc9fb3f9a
XDestination <XDestination@users.noreply.github.com>
2016-02-27 13:33:40 +0100

    Update heurigen.js

--- heurigen.js
+++ heurigen.js
@@ -1,16 +1,146 @@
+var _ = require('lodash');
 
 function HeurigenClient(config) {
   this.rest_client = config.rest_client;
   this.db_client = config.db_client;
+  this.allowed_cmds = [
+    'searchloc', 'searchname'
+  ];
   
   this.handleRequest = function(obj) {
     if (!this.isValidRequest(obj)) {
       return;
     }
+    
+    var chat_id = this.getChatId(obj);
+    var message_id = this.getMessageId(obj);
+    var key = this.getRequestKey(obj);
+    var cmd = this.getRequestCmd(obj);
+    
+    if (cmd !== null) {
+      switch (cmd.cmd) {
+        case 'searchloc':
+          if (cmd.param.length) {
+            // respond with typing
+            this.respondWaiting(chat_id, 'typing');
+            
+            // resolve location provided as param
+            
+            // request heurigens
+            
+            // respond with text
+          } else {
+            // ask for location
+            this.respond(chat_id, "Please provide a location to look for", message_id, {force_reply: true, selective: true});
+            
+            var obj = {};
+            this.db_client.set(key, JSON.stringify());
+            this.db_client.expireat(key, (new Date()).getTime() / 1000 + 300);
+          }
+          break;
+        case 'searchname';
+          if (cmd.param.length) {
+            // ask for location
+            this.respond(chat_id, "Please provide a location to look for", message_id, {force_reply: true, selective: true});
+          } else {
+            // ask for name to look for
+            this.respond(chat_id, "Please provide a name to look for", message_id, {force_reply: true, selective: true});
+          }
+          break;
+        default:
+          // respond with unknown cmd
+          this.respond(chat_id, "You entered an unknown command!", message_id);
+          
+          // delete entry for key
+          this.db_client.expireat(key, 0);
+          break;
+      }
+    } else {
+      this.db_client.get(key, function(resp) {
+        if (resp === null) {
+          
+        } else {
+          
+        }
+      });
+    }
   };
   
   this.isValidRequest = function(obj) {
-    return false;
+    if (!_.isUndefined(obj.update_id) && !_.isUndefined(obj.message)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  this.getChatId = function(obj) {
+    return obj.message.chat.id;
+  };
+  
+  this.getMessageId = function(obj) {
+    return obj.message.message_id;
+  };
+  
+  this.getRequestKey = function(obj) {
+    return obj.message.from.id + '#' + obj.message.chat.id;
+  };
+  
+  this.getRequestCmd = function(obj) {
+    if (!_.isUndefined(obj.message.text)) {
+      for (var i in this.allowed_cmds) {
+        if (obj.message.text.match(new RegExp('^/' + this.allowed_cmds[i]))) {
+          var cmd = this.allowed_cmds[i];
+          var param = obj.message.text.substr(obj.message.text.indexOf(' ')+1);
+          
+          return {
+            cmd: cmd,
+            param: param
+          };
+        }
+      } 
+    }
+
+    return null;
+  };
+  
+  this.respondWaiting = function(chat_id, action) {
+    var params = {
+      chat_id: chat_id,
+      action: action
+    };
+    
+    console.log('Sending ChatAction "' + action + '"');
+    this.rest_client.post('/sendChatAction', params, function(err, req, res) {
+      if (err) {
+        console.log('Sending ChatAction failed: ' + err);
+      } else {
+        console.log('Sending ChatAction succeeded: ' + res);
+      }
+    });
+  };
+  
+  this.respond = function(chat_id, text, reply_to_message_id, reply_markup) {
+    var params = {
+      chat_id: chat_id,
+      text: text
+    };
+    
+    if (!_.isUndefined(reply_to_message_id)) {
+      params.reply_to_message_id = reply_to_message_id;
+    }
+    if (!_.isUndefined(reply_markup)) {
+      params.reply_markup = reply_markup;
+    }
+    
+    console.log('Sending Request: ' + JSON.stringify(params));
+    this.rest_client.post('/sendMessage', params, function(err, req, res) {
+      if (err) {
+        console.log('Sending Request failed: ' + err);
+      } else {
+        console.log('Sending Request succeeded: ' + res);
+      }
+    });
   };
 }
 
