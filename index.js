541b7679456e66a4a610ae73f3e3e4f2222c01da
XDestination <XDestination@users.noreply.github.com>
2016-02-27 11:14:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,26 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
  
-server.get('/echo/:name', function (req, res, next) {
+server.get('/webhook/' + config.telegram.token, function (req, res, next) {
   res.json(200, req.params);
   return next();
 });
  
 server.listen(config.port, function () {
   console.log('%s listening at %s', server.name, server.url);
+  
+  // setup the webhook
+  console.log('Webhook setup succeeded');
+});
+
+server.on('close', function() {
+  console.log('Stopping ...');
+  
+  // unregister webhook
+  console.log('Unregister Webhook succeeded');
+});
+
+// handle killing of server
+process.on('SIGINT', function() {
+  server.close();
 });
