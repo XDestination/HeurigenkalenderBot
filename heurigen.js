5d476c5cf6dde35cf90bc2a078e3ce3bf9aba4a1
XDestination <XDestination@users.noreply.github.com>
2016-02-27 13:55:19 +0100

    Update heurigen.js

--- heurigen.js
+++ heurigen.js
@@ -20,8 +20,11 @@ function HeurigenClient(config) {
     this.db_client.get(cache_key, function(resp) {
       console.log('Cache response: ' + resp);
       if (resp === null) {
-        this.db_client.set(cache_key, 1);
-        this.db_client.expireat(cache_key, (new Date()).getTime()/1000 + 86400);
+        this.db_client.set(cache_key, 1, function(success) {
+          if (success) {
+            this.db_client.expireat(cache_key, (new Date()).getTime()/1000 + 86400);
+          }
+        });
         
         var chat_id = this.getChatId(obj);
         var message_id = this.getMessageId(obj);
