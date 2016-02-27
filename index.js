0dad4136958b904f44e078980415b7b1fde257a4
XDestination <XDestination@users.noreply.github.com>
2016-02-27 11:38:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 var restify = require('restify');
 var fs = require('fs');
+var request = require('request');
 
 var config = require('./config');
 
+// setup a client to be used to do requests
+var client = restify.createJsonClient({
+  url: config.telegram.baseurl + '/bot' + config.telegram.token,
+  version: '*'
+});
 
+function updatewebhook(set_webhook) {
+  var url = config.telegram.baseurl + '/bot' + config.telegram.token + '/setWebhook';
+  var formdata = {
+    url: set_webhook ? 'https://' + config.domain + '/webhook/' + config.telegram.token : '',
+    certificate: set_webhook ? fs.readFileSync(config.certpath + '/' + config.domain + '.pem') : ''
+  };
+  
+  var req = request.post({url: url, formData: formdata}, function (err, resp, body) {
+    if (err) {
+      if (set_webhook) {
+        console.log('Setting the Webhook failed');
+      } else {
+        console.log('Unsetting the Webhook failed');
+      }
+      console.log(err);
+    } else {
+      if (set_webhook) {
+        console.log('Setting the Webhook succeeded');
+      } else {
+        console.log('Unsetting the Webhook succeeded');
+      }
+      console.log(resp);
+      console.log(body);
+    }
+  });
+}
+
+// setup the server that receives the webhooks
 var server = restify.createServer({
   name: config.app,
   version: config.version,
@@ -23,14 +57,14 @@ server.listen(config.port, function () {
   console.log('%s listening at %s', server.name, server.url);
   
   // setup the webhook
-  console.log('Webhook setup succeeded');
+  updatewebhook(true);
 });
 
 server.on('close', function() {
   console.log('Stopping ...');
   
   // unregister webhook
-  console.log('Unregister Webhook succeeded');
+  updatewebhook(false);
 });
 
 // handle killing of server
